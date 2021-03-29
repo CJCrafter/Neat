c0a5f3815e6b320353e989ba6226c7a66bc45c15
CJCrafter <collinjbarber@gmail.com>
2021-03-29 05:02:13 -0700

    Git-Github link test

--- src/main/java/me/cjcrafter/neat/Runner.java
+++ src/main/java/me/cjcrafter/neat/Runner.java
@@ -5,7 +5,5 @@ public class Runner {
     public static void main(String[] args) {
         Neat neat = new Neat(2, 1, 200);
         neat.temp();
-
-        //new Frame().setGenome(neat.newGenome());
     }
 }
